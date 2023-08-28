8ba36004abde5fdf73d1d3e50b25bd4c2c3337c2
Jangwoo Park <p.jangwoo@gmail.com>
2023-08-28 20:03:16 +0900

    8월 28일

---
+++ frontend/src/Components/app/Header.js
@@ -0,0 +1,11 @@
+function Header() {
+	return (
+		<header className="py-4">
+			<div className="container text-center">
+				<img alt="" src="/images/gallery.jpg" width="1100" height="150" />
+			</div>
+		</header>
+	);
+}
+
+export default Header;
---
+++ frontend/src/Components/app/Home.js
@@ -0,0 +1,10 @@
+
+function Home() {
+	return (
+		<div>
+			<h2> 게시판 Home 입니다. </h2>
+		</div>
+	);
+}
+
+export default Home;
